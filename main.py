c16cd84f2b2d57d7d943a417c66ba939966c99c5
ljmocic <ljubisa.mocic@gmail.com>
2018-01-26 19:03:42 +0100

    Debugging info and parameters tuning

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import numpy as np
 import time as t
 import cv2
 import pyocr
+import threading
+import time
+
 
 # setup tesseract
 tools = pyocr.get_available_tools()
@@ -14,20 +17,21 @@ builder = pyocr.builders.DigitBuilder()
 # 8 Treat the image as a single word.
 builder.tesseract_flags = ['-psm', '8']
 
-# score detected by tesseract ocr
 highscore = 0
 iteration = 0
-states = 20
+episode = 0
+states = 17
 active_state = 0
-actions_array = [SHIFT, A, D]
-Q = np.zeros([states, len(actions_array)])
+actions_array = [A, SHIFT, D]
+# Q = np.zeros([states, len(actions_array)])
+Q = 100 * np.random.rand(states, len(actions_array))
 
 # learning parameters
-lr = 0.25
-y = 0.3
+lr = 0.5
+y = 0.5
 
 # settings
-keypress_pause = 0.1
+keypress_pause = 0.3
 
 
 def remove_comma(detected_score):
@@ -61,26 +65,40 @@ def process_frame(img):
     return img
 
 
+def print_action(chosen_aciton):
+    print("Action: ")
+    if chosen_aciton == 0:
+        print("<-")
+    elif chosen_aciton == 1:
+        print("^")
+    elif chosen_aciton == 2:
+        print("->")
+
 def run_q_algorithm():
-    global active_state
+    global active_state, highscore, episode
     chosen_action = np.argmax(Q[active_state, :])
-
+    print("value: ")
+    print(Q[active_state, chosen_action])
     control_car(chosen_action)
+    print_action(chosen_action)
     reward = update_reward()
-
     Q[active_state, chosen_action] = (1 - lr) * Q[active_state, chosen_action] + lr * (reward + y * np.max(Q[(active_state + 1), :]))
-    # print(Q)
+    print(Q)
 
     # if it is not final state, update active state, otherwise reset race
     if active_state + 2 != states:
         active_state += 1
     else:
         reset_race()
+        episode += 1
+        print("Episode " + str(episode))
+        np.savetxt("qmatrix" + str(episode) + ".out", Q, delimiter=',')
         active_state = 0
         highscore = 0
 
 
 def update_reward():
+    global highscore
 
     # grab piece of screen for processing
     screenshot = np.array(ImageGrab.grab(bbox=(650, 300, 710, 330)))
@@ -99,6 +117,8 @@ def update_reward():
         print(detected_number)
         highscore += detected_number
         reward = detected_number
+
+    print("Reward:" + reward)
 
     # Show detection frame for debugging
     cv2.imshow("frame", img)
@@ -123,15 +143,27 @@ def reset_race():
     ReleaseKey(ENTER)
     t.sleep(3 * keypress_pause)
 
+'''
+def looper():    
+    # i as interval in seconds    
+    threading.Timer(10, looper).start()   
+    # iterations per minute
+    print(iteration / 60)
+    global iteration
+    iteration = 0
+
+looper()
+'''
+
 
 while(True):
     run_q_algorithm()
-    # print("Iteration" + str(iteration))
+    print("Iteration" + str(iteration))
     iteration += 1
-
+    
     key = cv2.waitKey(1)
     if key == 27:
-        for action in actions:
+        for action in actions_array:
             ReleaseKey(action)
         break
 
